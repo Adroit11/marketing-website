8a1c15d0460d2edea65003c1eb6ffb59ef8f85ac
Lawrence <97756376+lawrdev@users.noreply.github.com>
2024-08-20 02:31:30 +0100

    fix:

--- src/components/landing/faqs.tsx
+++ src/components/landing/faqs.tsx
@@ -8,9 +8,22 @@ export const FAQS = () => {
           <h6 className="mb-1 header-6 text-center">
             Frequently Asked Questions
           </h6>
-          <h2 className="mb-16 header-2 text-center w-fit mx-auto">
+          <h2 className="mb-8 header-2 text-center w-fit mx-auto">
             Do you have any questions?
           </h2>
+
+          <div className="mb-16 w-full grid grid-cols-1 sm:grid-cols-2 gap-x-8 gap-y-6 max-w-4xl">
+            <div className="p-6 border border-slate-100/20 rounded-xl">
+              <p className="font-semibold">Email:</p>
+              <a className="text-primary" href="mailto:technology@joinramp.co">
+                technology@joinramp.co
+              </a>
+            </div>
+            <div className="p-6 border border-slate-100/20 rounded-xl">
+              <p className="font-semibold">Phone:</p>
+              <p className="text-primary">02018891783</p>
+            </div>
+          </div>
 
           <Button className="px-7" size={"lg"}>
             Get in Touch
--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -7,7 +7,7 @@ export const LandingHero = () => {
       <div className="container">
         <div className="flex flex-col items-center text-center pt-24 pb-10">
           <h1 className="mb-5 text-4xl lg:text-6xl font-semibold max-w-3xl tracking-normal !leading-[110%]">
-            The best software partner for your business
+            New Customers, More Sales
           </h1>
           <p className="mb-10 text-lg lg:text-xl text-foreground/70 max-w-2xl">
             Set up your store, get access to a smooth payment flow and open up a
@@ -18,9 +18,9 @@ export const LandingHero = () => {
             <Button className="px-14 py-6 text-base" size={"lg"}>
               Get Started
             </Button>
-            <Button className="text-base py-6" variant={"link"} size={"lg"}>
+            {/* <Button className="text-base py-6" variant={"link"} size={"lg"}>
               View Products
-            </Button>
+            </Button> */}
           </div>
 
           <div className="w-fit shadow-lg rounded-lg overflow-hidden">
