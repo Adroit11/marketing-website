6eecd1004066a5ef1c6c29fc235c75817e16a305
Lawrence <97756376+lawrdev@users.noreply.github.com>
2024-08-23 23:39:01 +0100

    fix:

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,18 @@ const fontSans = FontSans({
 export const metadata: Metadata = {
   title: "Ramp | New Customers, More Sales",
   description: "Ramp | New Customers, More Sales",
+  openGraph: {
+    title: "Ramp | New Customers, More Sales",
+    description: "Ramp | New Customers, More Sales",
+    siteName: "Ramp",
+    images: {
+      url: "http://res.cloudinary.com/dc9kfp5gt/image/upload/v1724451002/RampIcon/nk4lnmcd8vhwhet6mvn8.svg",
+      width: 800,
+      height: 600,
+    },
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
